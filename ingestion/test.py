9e75ee0e9e53a19e426138856cedcc4241c7ece4
Thiago Alves <avsthiago@gmail.com>
2024-10-06 16:03:26 +0200

    First commit ingestion lambda

---
+++ ingestion/test.py
@@ -0,0 +1,23 @@
+import os
+
+os.environ["LANCEDB_S3_BUCKET"] = "house-rag-vector-db-xxxxx"
+os.environ["LANCEDB_S3_PATH"] = "house-rag/database"
+os.environ["DYNAMODB_TABLE_NAME"] = "house-rag-commit-vector-db"
+os.environ["AWS_REGION"] = "eu-central-1"
+os.environ["LOG_LEVEL"] = "DEBUG"
+
+event = {
+    "Records": [
+        {
+            "s3": {
+                "bucket": {"name": "house-rag-data-source-ex4hbt"},
+                "object": {"key": "house-rag/test file.pdf"},
+            }
+        }
+    ]
+}
+
+
+import handler
+
+handler.lambda_handler(event, None)
